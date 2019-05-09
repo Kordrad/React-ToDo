d58ca20ed4f9f10bf313f816de1e27555d6d512f
Kordrad <kord.stp@gmail.com>
2019-05-09 21:04:56 +0200

    Structure js ToDo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React ,{ Component } from 'react';
 
 import './App.css';
+import ToDo from "./todo";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                
+                <ToDo/>
             </div>
         );
     }
