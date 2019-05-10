507b6eb5749209e58370e5e2d2305240c377312c
Kordrad <kord.stp@gmail.com>
2019-05-10 12:18:09 +0200

    Styled app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React ,{ Component } from 'react';
 
 import './App.css';
 import ToDo from "./todo";
+import styled, { css } from 'styled-components'
+
+const StyledApp = styled.div`
+  padding: 75px 0;
+  & > * {
+    margin: 0 auto;
+    width: 300px;
+    min-height: 500px;
+    box-sizing: border-box;
+  }
+`;
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <StyledApp className="App">
                 <ToDo/>
-            </div>
+            </StyledApp>
         );
     }
 }
